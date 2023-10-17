f1a874ccb54557860bb8015b0c74e6391328b350
Hatem Jaber <hatem@7079.net>
2023-10-17 09:06:11 -0400

    added version and version prompty to tag and push script

--- tag_and_push.sh
+++ tag_and_push.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-# Check if version argument was passed
-if [ -z "$1" ]; then
-  echo "Usage: $0 <version>"
-  exit 1
+# Function to increment version
+increment_version() {
+  echo "$1" | awk -F. '{$NF = $NF + 1;} 1' OFS=.
+}
+
+# Check if .version file exists
+if [ -f ./.version ]; then
+  # Read the last version from the .version file
+  LAST_VERSION=$(cat ./.version)
+  # Increment the version number
+  VERSION=$(increment_version $LAST_VERSION)
+else
+  # Prompt for a version number if .version file doesn't exist
+  echo "Enter the initial version number (e.g., 1.0.0): "
+  read VERSION
 fi
 
-# Set the version number
-VERSION=$1
+# Confirm version
+echo "Using version: $VERSION. Is this okay? (y/n)"
+read CONFIRM
+if [ "$CONFIRM" != "y" ]; then
+  echo "Aborted."
+  exit 1
+fi
 
 # Build the Docker image
 docker build -t hatemjaber/tor-rotator:$VERSION .
@@ -31,5 +47,8 @@ if [ $? -ne 0 ]; then
   echo "Docker push failed for latest."
   exit 1
 fi
+
+# Update the .version file with the new version
+echo $VERSION > ./.version
 
 echo "Successfully built and pushed hatemjaber/tor-rotator:$VERSION and latest."
